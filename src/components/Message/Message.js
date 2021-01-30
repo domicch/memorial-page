5b23535c1f34ad3383099565e47942815668c59e
domicch <domicch@gmail.com>
2021-01-30 16:13:47 +0000

    1. deploy to Firebase 2. use resized image in messages

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -31,7 +31,7 @@ const Message = (props) => {
             <Grid container justify="center">
                 <Grid item xs={12} sm={10} md={8}>
                     <ImageCard
-                        imageURL={props.originalImageURL}
+                        imageURL={props.resizedImageURL}
                     />
                 </Grid>
             </Grid>
